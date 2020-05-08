77072f896070e8710473bbc28faa9a66f89bafe0
Azorji Kelechi Oliver <kelechioliver96@gmail.com>
2020-05-08 20:20:48 +0100

    chore: Setup linting  - add linting packages  - add linting config file  - update package.json to include a lint script  - update github workflow to check linting on every push
    finishes #8

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,15 +1,15 @@
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 
-//selecting localstorage
+// selecting localstorage
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
-	key: 'root',
-	storage,
-	whitelist: [], // what we want to persist
+  key: 'root',
+  storage,
+  whitelist: [] // what we want to persist
 };
 
 const rootReducer = combineReducers({});
 
-export default persistReducer(persistConfig, rootReducer)
+export default persistReducer(persistConfig, rootReducer);
